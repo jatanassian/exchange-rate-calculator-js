457f5defe595aa59eed872dfbb46e4b5daad9768
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-20 09:36:56 -0400

    get the needed DOM elements and add event listeners that call a calculate function

---
+++ script.js
@@ -0,0 +1,19 @@
+const currencyEl_one = document.getElementById('currency-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rate and update the DOM
+const calculate = () => {
+  console.log('test');
+}
+
+// Event listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+
+calculate();
